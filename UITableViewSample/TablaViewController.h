3806431004a0f916b135ba8c4d6ffa4b7d1198b7
Daniel Alvarez <i@danyalvarez.com>
2014-02-10 00:26:30 -0400

    First commit

---
+++ UITableViewSample/TablaViewController.h
@@ -0,0 +1,13 @@
+//
+//  TablaViewController.h
+//  UITableViewSample
+//
+//  Created by Daniel Alvarez on 09/02/14.
+//  Copyright (c) 2014 Daniel Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TablaViewController : UITableViewController
+
+@end
